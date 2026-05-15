042eb9ca3d1b70671518999d99e9cf3e386b8016
Rohit Sali <155815075+Rohit2sali@users.noreply.github.com>
2026-05-15 23:37:55 +0530

    Create s3_utils.py

---
+++ app/shared/s3_utils.py
@@ -0,0 +1,42 @@
+import boto3
+import os
+
+from first_aws.app.core.config import AWS_BUCKET_NAME
+
+s3 = boto3.client("s3")
+
+
+MODEL_PREFIX = "tinyllama-weights/"
+MODEL_DIR = "/code/models/tinyllama-weights"
+
+
+def download_model():
+
+    os.makedirs(MODEL_DIR, exist_ok=True)
+
+    response = s3.list_objects_v2(
+        Bucket=AWS_BUCKET_NAME,
+        Prefix=MODEL_PREFIX,
+    )
+
+    for obj in response.get("Contents", []):
+
+        key = obj["Key"]
+
+        if key.endswith("/"):
+            continue
+
+        filename = key.split("/")[-1]
+
+        local_path = os.path.join(MODEL_DIR, filename)
+
+        print(f"Downloading {key} -> {local_path}")
+
+        s3.download_file(
+            AWS_BUCKET_NAME,
+            key,
+            local_path,
+        )
+
+    print("Model downloaded")
+
