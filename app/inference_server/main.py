e2805cf36bff6fd38e52e971af956b78cab7642a
Rohit Sali <155815075+Rohit2sali@users.noreply.github.com>
2026-05-15 23:48:37 +0530

    Update main.py

--- app/inference_server/main.py
+++ app/inference_server/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-from first_aws.inference_server.model_loader import tokenizer, model
+from model_loader import tokenizer, model
 
 app = FastAPI()
 
