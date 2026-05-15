eab08427602a3bc72d4512607cc61c46f717574e
Rohit Sali <155815075+Rohit2sali@users.noreply.github.com>
2026-05-15 23:34:25 +0530

    Update main.py

--- app/inference_server/main.py
+++ app/inference_server/main.py
@@ -1,1 +1,33 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
 
+from first_aws.inference_server.model_loader import tokenizer, model
+
+app = FastAPI()
+
+
+class PromptRequest(BaseModel):
+    prompt: str
+
+
+@app.post("/generate")
+def generate(data: PromptRequest):
+
+    inputs = tokenizer(
+        data.prompt,
+        return_tensors="pt"
+    )
+
+    output = model.generate(
+        **inputs,
+        max_new_tokens=50
+    )
+
+    response = tokenizer.decode(
+        output[0],
+        skip_special_tokens=True
+    )
+
+    return {
+        "response": response
+    }
