b0d83d4f6b3511dc638a8140d0123662476bcff0
Rohit Sali <155815075+Rohit2sali@users.noreply.github.com>
2026-05-17 12:26:44 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
-
 from app.db.database import engine
 from app.db.models import Base
-
 from app.auth.routes import router as auth_router
 from app.chat.routes import router as chat_router
 from app.uploads.routes import router as upload_router
 
-
-
 app = FastAPI()
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
 
 templates = Jinja2Templates(directory="templates")
 
@@ -32,33 +32,5 @@ async def home(request: Request):
         request,
         "index.html"
     )
-# from fastapi import FastAPI
-
-# from app.db.database import engine
-# from app.db.models import Base
-
-# from app.auth.routes import router as auth_router
-# from app.chat.routes import router as chat_router
-# from app.uploads.routes import router as upload_router
-
-# from app.aws.s3_utils import download_model
 
 
-# app = FastAPI()
-
-
-# @app.on_event("startup")
-# def startup_event():
-
-#     Base.metadata.create_all(bind=engine)
-
-#     download_model()
-
-# app.include_router(auth_router)
-# app.include_router(chat_router)
-# app.include_router(upload_router)
-
-
-# @app.get("/")
-# def root():
-#     return {"message": "AI Backend Running"}
