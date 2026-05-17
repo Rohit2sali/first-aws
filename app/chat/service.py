f2e9fbb37971835b87908936db7b9318ec05e4b9
Rohit Sali <155815075+Rohit2sali@users.noreply.github.com>
2026-05-17 12:24:22 +0530

    Update service.py

--- app/chat/service.py
+++ app/chat/service.py
@@ -4,18 +4,29 @@ import requests
 INFERENCE_URL = "http://inference-server:9000/generate"
 
 def process_chat(prompt: str):
+    data = {
+        "prompt": prompt
+    }
+    for attempt in range(5):
+        try:
+            response = requests.post(
+                INFERENCE_URL,
+                json=data,
+                timeout=120
+            )
 
-    response = requests.post(
-        INFERENCE_URL,
-        json={
-            "prompt": prompt
-        },
-        timeout=120
-    )
+            print(response.status_code)
+            print(response.text)
+        
+            data = response.json()
+        
+            return data["response"]
 
-    print(response.status_code)
-    print(response.text)
+        except requests.exceptions.RequestException as e:
+            print(f"Attempt {attempt + 1} failed: {e}")
 
-    data = response.json()
+            # last attempt
+            if attempt == 4:
+                raise Exception("Inference server unavailable after retries")
 
-    return data["response"]
+            time.sleep(3)
